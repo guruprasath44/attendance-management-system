1786d581ac0b7c0138e844458bce7b3b2bd2547d
Guruprasath4401 <guruprasathindirakumar@gmail.com>
2021-07-25 12:27:16 +0530

    Code Refactoring

--- controller/routes/route.js
+++ controller/routes/route.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const moment = require('moment');
 const connection = require('../../model/database');
-
 const router = express.Router();
 let username = '';
 let mail = '';
+let pass = '';
 let input = '';
 router.get('/', (req, res) => {
   console.log('Hi login');
-  res.render('login');
+  res.render('login', { success: '' });
 });
 router.get('/Register', (req, res) => {
   console.log('Hi Register');
   res.render('register');
 });
-router.use('/Login', (req, res) => {
+router.get('/Login', (req, res) => {
   console.log('Hi login');
-  res.render('login');
+  res.render('login', { success: '' });
 });
-
 /* admin */
 router.get('/admin/dashboard', (req, res) => {
   res.render('admin_dashboard', { userData: username });
 });
-
 router.get('/admin/add_faculty', (req, res) => {
   res.render('add_faculty', { userData: username, success: '' });
 });
@@ -31,23 +29,32 @@ router.get('/admin/add_student', (req, res) => {
   res.render('add_student', { userData: username, success: '' });
 });
 router.get('/admin/add_subject', (req, res) => {
-  res.render('add_subject', { userData: username, success: '' });
+  connection.query('select firstname,lastname from faculty ', (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.render('add_subject', {
+        userData: username, faculty: results, success: ''
+      });
+    }
+  });
 });
-
 router.get('/student/subject_info', (req, res) => {
   res.render('subject_info', { userData: username, subjectData: '', sem: '' });
 });
-// router.get('/student/student_profile',function(req,res){
-//     res.render( "student_profile",{name: username,success:''});
-// })
-
 router.get('/admin/faculty_subject', (req, res) => {
   res.render('faculty_subject', {
     userData: username, subjectData: '', sem: '', success: '',
   });
 });
 router.get('/admin/mark_attendance', (req, res) => {
-  res.render('mark_attendance', { userData: username, success: '' });
+  connection.query('select sub_code from subject order by sem ', (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.render('mark_attendance', {
+        userData: username, subject_code: results, success: '',
+      });
+    }
+  });
 });
 router.post('/admin/mark', (req, res) => {
   const { semester } = req.body;
@@ -71,30 +78,59 @@ router.post('/admin/mark', (req, res) => {
       console.log(sub);
     }
   });
-
-
 });
-
 router.get('/admin/edit_attendance', (req, res) => {
-  res.render('edit_attendance', { userData: username,success: '' });
+  connection.query('select sub_code from subject order by sem ', (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.render('edit_attendance', {
+        userData: username, subject_code: results, success: '',
+      });
+    }
+  });
 });
 router.get('/attendance/report', (req, res) => {
-  res.render('report_generate', { userData: username });
+  connection.query('select sub_code from subject order by sem ', (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.render('report_generate', {
+        userData: username, subject_code: results, success: '',
+      });
+    }
+  });
 });
-
-router.put('/attendance/report/generate', (req, res) => {
+router.get('/student/report/period', (req, res) => {
+  connection.query('select sub_code from subject order by sem ', (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.render('student_attendance_period', {
+        userData: username, subject_code: results, success: '',
+      });
+    }
+  });
+});
+router.get('/student/report/student', (req, res) => {
+  connection.query('select sub_code from subject order by sem ', (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.render('student_attendance_student', {
+        userData: username, subject_code: results, success: '',
+      });
+    }
+  });
+});
+router.post('/report/period', (req, res) => {
   const { semester } = req.body;
   const sub = req.body.sub_code;
   const { time } = req.body;
   const { date } = req.body;
-
   connection.query('select  rollno from student order by rollno ', (err, results) => {
     if (err) throw err;
     if (results.length > 0) {
       console.log('Reports displayed');
       connection.query('select  *  from attendance  where semester=? && time=? && subCode=? && date=?  ', [semester, time, sub, date], (err, result) => {
-        console.log(results[0].rollno);
-        const arr = [{ r1: result[0]._18IT01, r2: results[0].rollno },
+        if (results.lenth > 0) {
+          const arr = [{ r1: result[0]._18IT01, r2: results[0].rollno },
           { r1: result[0]._18IT02, r2: results[1].rollno },
           { r1: result[0]._18IT03, r2: results[2].rollno },
           { r1: result[0]._18IT04, r2: results[3].rollno },
@@ -127,21 +163,91 @@ router.put('/attendance/report/generate', (req, res) => {
           { r1: result[0]._18IT32, r2: results[30].rollno },
           { r1: result[0]._18IT33, r2: results[31].rollno },
           { r1: result[0]._18IT34, r2: results[32].rollno },
+          ];
+          console.log(arr);
 
-        ];
-
-        console.log(arr);
-        if (err) throw err;
-        if (result.length > 0) {
-          res.render('report', {
+          res.render('students_report_period', {
             report: results, userData: username, colvalue: result, attendance: arr,
           });
+
+        } else {
+          connection.query('select sub_code from subject order by sem ', (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+              res.render('student_attendance_period', {
+                userData: username, subject_code: results, success: 'Invalid details',
+              });
+            }
+          });
         }
+        if (err) throw err;
       });
     }
   });
 });
-
+router.post('/attendance/report/generate', (req, res) => {
+  const { semester } = req.body;
+  const sub = req.body.sub_code;
+  const { time } = req.body;
+  const { date } = req.body;
+  connection.query('select  rollno from student order by rollno ', (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      connection.query('select  *  from attendance  where semester=? && time=? && subCode=? && date=?  ', [semester, time, sub, date], (err, result) => {
+        if (result.length > 0) {
+          const arr = [{ r1: result[0]._18IT01, r2: results[0].rollno },
+          { r1: result[0]._18IT02, r2: results[1].rollno },
+          { r1: result[0]._18IT03, r2: results[2].rollno },
+          { r1: result[0]._18IT04, r2: results[3].rollno },
+          { r1: result[0]._18IT05, r2: results[4].rollno },
+          { r1: result[0]._18IT06, r2: results[5].rollno },
+          { r1: result[0]._18IT07, r2: results[6].rollno },
+          { r1: result[0]._18IT08, r2: results[7].rollno },
+          { r1: result[0]._18IT09, r2: results[8].rollno },
+          { r1: result[0]._18IT10, r2: results[9].rollno },
+          { r1: result[0]._18IT11, r2: results[10].rollno },
+          { r1: result[0]._18IT12, r2: results[11].rollno },
+          { r1: result[0]._18IT13, r2: results[12].rollno },
+          { r1: result[0]._18IT14, r2: results[13].rollno },
+          { r1: result[0]._18IT15, r2: results[14].rollno },
+          { r1: result[0]._18IT16, r2: results[15].rollno },
+          { r1: result[0]._18IT17, r2: results[16].rollno },
+          { r1: result[0]._18IT18, r2: results[17].rollno },
+          { r1: result[0]._18IT19, r2: results[18].rollno },
+          { r1: result[0]._18IT20, r2: results[19].rollno },
+          { r1: result[0]._18IT21, r2: results[20].rollno },
+          { r1: result[0]._18IT22, r2: results[21].rollno },
+          { r1: result[0]._18IT23, r2: results[22].rollno },
+          { r1: result[0]._18IT25, r2: results[23].rollno },
+          { r1: result[0]._18IT26, r2: results[24].rollno },
+          { r1: result[0]._18IT27, r2: results[25].rollno },
+          { r1: result[0]._18IT28, r2: results[26].rollno },
+          { r1: result[0]._18IT29, r2: results[27].rollno },
+          { r1: result[0]._18IT30, r2: results[28].rollno },
+          { r1: result[0]._18IT31, r2: results[29].rollno },
+          { r1: result[0]._18IT32, r2: results[30].rollno },
+          { r1: result[0]._18IT33, r2: results[31].rollno },
+          { r1: result[0]._18IT34, r2: results[32].rollno },
+          ];
+          console.log(arr);
+          res.render('report', {
+            report: results, userData: username, colvalue: result, attendance: arr,
+          });
+        } else {
+          connection.query('select sub_code from subject order by sem ', (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+              res.render('report_generate', {
+                userData: username, subject_code: result, success: 'Invalid details',
+              });
+            }
+          });
+        }
+        if (err) throw err;
+      });
+    }
+  });
+});
 router.post('/admin/attendence/submit/:sem/:time/:sub/:date', (req, res) => {
   const att = req.body.SelectedValues;
   const r0 = req.body.att[0]; const r1 = req.body.att[1]; const r2 = req.body.att[2]; const r3 = req.body.att[3];
@@ -155,7 +261,6 @@ router.post('/admin/attendence/submit/:sem/:time/:sub/:date', (req, res) => {
   const r33 = req.body.att[32];
   console.log(req.body);
   const values = [r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r25, r26, r27, r28, r29, r30, r31, r32, r33, req.params.sem, req.params.time, req.params.sub, req.params.date];
-
   const sql = 'update attendance set _18IT01=?, _18IT02=? ,_18IT03=?, _18IT04=?, _18IT05=?, _18IT06=?, _18IT07=?, _18IT08=?, _18IT09=?, _18IT10=?, _18IT11=?, _18IT12=?, _18IT13=?, _18IT14=?, _18IT15=?,_18IT16=?, _18IT17=?,_18IT18=?,_18IT19=?,_18IT20=?,_18IT21=?,_18IT22=?,_18IT23=?,_18IT25=?,_18IT26=?,_18IT27=?,_18IT28=?,_18IT29=?,_18IT30=?,_18IT31=?,_18IT32=?,_18IT33=?,_18IT34=?  where semester=? && time=? && subCode=? && date=? ';
   connection.query(sql, values, (err, result) => {
     if (err) {
@@ -166,7 +271,6 @@ router.post('/admin/attendence/submit/:sem/:time/:sub/:date', (req, res) => {
     res.render('mark_attendance', { userData: username, success: 'Attendance marked successfully' });
   });
 });
-
 router.get('/admin/subject/edit/:sub_code', (req, res) => {
   connection.query('select * from subject where sub_code=?', [req.params.sub_code], (err, results) => {
     if (err) throw err;
@@ -195,7 +299,6 @@ router.get('/student/class_student', (req, res) => {
     }
   });
 });
-
 router.get('/admin/class_admin', (req, res) => {
   connection.query('select  count(*) as strength  from student ', (err, count) => {
     if (err) throw err;
@@ -215,7 +318,6 @@ router.get('/admin/class_admin', (req, res) => {
     }
   });
 });
-
 router.post('/admin/subject/edit', (req, res, next) => {
   const { subcode } = req.body;
   const { subname } = req.body;
@@ -230,78 +332,64 @@ router.post('/admin/subject/edit', (req, res, next) => {
   });
   connection.query('select * from subject where sub_code=?', [subcode], (err, results) => {
     if (err) throw err;
-
     if (results.length > 0) {
       console.log('Subject Selected');
       console.log(results);
-
       res.render('edit_subject', { userData: username, subjectData: results, success: 'Updated Successfully' });
     }
   });
 });
-
 router.get('/admin/subject/delete/:sub_code', (req, res) => {
   connection.query('delete from subject where sub_code=?', [req.params.sub_code], (err, results) => {
     if (err) throw err;
-
     console.log('subject deleted');
     const sem = input;
     connection.query('select * from subject where sem=? order by credit DESC', [sem], (err, results) => {
       if (err) throw err;
       if (results.length > 0) {
         console.log('Subject Selected');
-
         res.render('faculty_subject', { subjectData: results, userData: username, sem: input });
       }
     });
   });
 });
-
 /* Student */
 router.get('/student/dashboard', (req, res) => {
   res.render('student_dashboard', { userData: username });
 });
-
 router.get('/admin/students_list', (req, res) => {
   connection.query('select * from student order by rollno ', (err, results) => {
     if (err) throw err;
     if (results.length > 0) {
       console.log('Students records displayed');
-
       res.render('students_list', { studentData: results, userData: username });
     }
   });
 });
-
 router.post('/admin/semester', (req, res) => {
   input = req.body.semester;
   connection.query('select * from subject where sem=? order by credit DESC', [input], (err, results) => {
     if (err) throw err;
-
     if (results.length > 0) {
       console.log('Subject Selected');
-
       res.render('faculty_subject', { subjectData: results, userData: username, sem: input });
     }
   });
 });
-
 router.post('/student/semester', (req, res) => {
   const inputsem = req.body.semester;
   connection.query('select * from subject where sem=?', [inputsem], (err, results) => {
     if (err) throw err;
     if (results.length > 0) {
       console.log('Subject Selected');
-
       res.render('subject_info', { subjectData: results, userData: username, sem: inputsem });
     }
   });
 });
-
+//LOGIN
 router.post('/user/login', (req, res) => {
-  const { email } = req.body;
-  const { password } = req.body;
-
+  const email = req.body.email;
+  const password = req.body.password;
   if (email && password) {
     const re = /\d/g;
     if (re.test(`${email}`)) {
@@ -311,7 +399,13 @@ router.post('/user/login', (req, res) => {
           console.log('Login Successfully');
           username = results[0].sname;
           mail = results[0].semail;
+          console.log(results);
           res.render('student_dashboard', { userData: username });
+
+        } else {
+          setTimeout(() => {
+            res.render('Login', { success: 'Please try Again' });
+          }, 2000);
         }
       });
     } else {
@@ -321,18 +415,21 @@ router.post('/user/login', (req, res) => {
           console.log('Login Successfully');
           username = results[0].fname;
           mail = results[0].femail;
-          console.log(mail);
+          console.log(results);
           res.render('admin_dashboard', { userData: username });
+        } else {
+          setTimeout(() => {
+            res.render('Login', { success: 'Please try Again' });
+          }, 2000);
         }
       });
     }
   }
-  // else{
-  //     res.render("Login",{er:'Please try Again'});
-  //  else{
-  //     res.render('Incorrect details');
-  //     // res.end();
-  // }
+  else {
+    setTimeout(() => {
+      res.render('Login', { success: 'Please try Again' });
+    }, 2000);
+  }
 });
 router.post('/user_signup', (req, res) => {
   const { name } = req.body;
@@ -345,7 +442,7 @@ router.post('/user_signup', (req, res) => {
       if (name.length != 0) {
         console.log('Values Inserted');
         setTimeout(() => {
-          res.render('/Login');
+          res.render('Login', { success: '' });
         }, 2000);
       } else {
         setTimeout(() => {
@@ -359,7 +456,7 @@ router.post('/user_signup', (req, res) => {
       if (name.length != 0) {
         console.log('Values Inserted');
         setTimeout(() => {
-          res.render('login', { userData: username });
+          res.render('Login', { userData: username, success: '' });
         }, 2000);
       } else {
         setTimeout(() => {
@@ -389,7 +486,6 @@ router.post('/add_student', (req, res) => {
     if (err) throw err;
     if (results) {
       console.log('Student Inserted');
-
       res.render('add_student', { userData: username, success: 'Successfully Added!' });
     }
   });
@@ -438,7 +534,6 @@ router.get('/student/profile', (req, res) => {
     if (err) throw err;
     if (results.length > 0) {
       console.log('My profile');
-
       username = results[0].firstname;
       res.render('student_profile', { userData: results, name: username, success: '' });
     }
@@ -449,13 +544,11 @@ router.get('/admin/faculty_profile', (req, res) => {
     if (err) throw err;
     if (results.length > 0) {
       console.log('My profile');
-
       username = results[0].firstname;
       res.render('faculty_profile', { userData: results, name: username, success: '' });
     }
   });
 });
-
 router.post('/admin/update', (req, res, next) => {
   const { fname } = req.body;
   const { lname } = req.body;
@@ -481,7 +574,6 @@ router.post('/admin/update', (req, res, next) => {
     });
   });
 });
-
 router.post('/student/update', (req, res, next) => {
   const { fname } = req.body;
   const { lname } = req.body;
@@ -507,13 +599,11 @@ router.post('/student/update', (req, res, next) => {
     });
   });
 });
-
 router.get('/admin/faculty', (req, res) => {
   connection.query('select * from faculty ', (err, results) => {
     if (err) throw err;
     if (results.length > 0) {
       console.log('Faculty List displayed');
-
       res.render('faculty', { facultyData: results, userData: username });
     }
   });
@@ -521,9 +611,7 @@ router.get('/admin/faculty', (req, res) => {
 router.get('/admin/faculty/delete/:email', (req, res) => {
   connection.query('delete from faculty where email=?', [req.params.email], (err, results) => {
     if (err) throw err;
-
     console.log('Faculty deleted');
-
     res.redirect('/admin/faculty');
   });
 });
